beacb78f6b52b1b3b9feaf21497fc57a93bed964
michael canady <michaeldcanady@yaho.com>
2021-01-10 12:49:51 -0500

    a

--- test.go
+++ test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
+	winapi "github.com/michaeldcanady/windows-api/windows-api"
 	"github.com/michaeldcanady/windows-api/windows-api/kernel32"
 )
 
@@ -14,13 +16,41 @@ func main() {
 	//	fmt.Println("Connection successful")
 	//}
 	//address := "\\\\?\\Volume{8B45C404-2738-4BC6-8A55-A2E401D87A35}\\"
-	ret, err := kernel32.GetVolumeNameForVolumeMountPointW("C:")
+	//ret, err := kernel32.GetVolumeNameForVolumeMountPointW("C:\\")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(ret)
+
+	//fmt.Println(kernel32.GetVolumeInformationW("Z:\\"))
+	fmt.Println(kernel32.GetDriveTypeW("\\\\fs3.liberty.edu\\hdbackups\\"))
+	handle := winapi.WNetOpenEnumW()
+	winapi.WNetEnumResourceW(handle)
+	//drive()
+	//kernel32.DeleteVolumeMountPointW("D:\\")
+	//winapi.WNetCancelConnection("\\\\fs3.liberty.edu\\hdbackups", true)
+}
+
+func drive() {
+	drives, err := kernel32.GetLogicalDrives()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(ret)
+	var remoteDrives []string
+	for _, drive := range drives {
+		drive = strings.ToUpper(drive)
 
-	//kernel32.GetVolumeInformationW("\\\\Share\\Share" or "C:\\")
+		if !strings.HasSuffix(drive, ":\\") {
+			drive = drive + ":\\"
+		}
 
-	fmt.Println(kernel32.GetLogicalDrives())
+		ty, err := kernel32.GetDriveTypeW(drive)
+		if err != nil {
+			panic(err)
+		}
+		if ty == "remote" {
+			remoteDrives = append(remoteDrives, drive)
+		}
+	}
+	fmt.Println(remoteDrives)
 }
